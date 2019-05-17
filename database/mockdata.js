a6ac0122e4c97cd4f696db84f629bbe506d51fff
Ramin Rabani <rrabani2@gmail.com>
2019-05-17 10:23:58 -0700

    Capitalize food names

--- database/mockdata.js
+++ database/mockdata.js
@@ -5,19 +5,19 @@ let reviews = [
 ];
 
 const commentGenerator = (foodType, starRating) => {
-  let italianFoods = ['cheese pizza', 'chicken parmesan', 'spaghetti and meatballs', 'garlic bread', 
-  'pasta', 'bread stick', 'Caesar salad', 'pepperoni pizza', 'bbq pizza'];
-  let mexicanFoods = ['fish tacos', 'chicken tacos', 'carne asada tacos', 
-  'spicy enchiladas', 'California burrito', 'shrimp tacos', 'fajita salad bowl', 'veggie bowl',
-   'steak quesadilla', 'nacho platter', 'taquitos'];
-  let chineseFoods = ['chow mein', 'chicken fried rice', 'orange chicken', 'teriyaki chicken', 
-  'wok-fired shrimp', 'kung pao chicken','veggie egg roll', 'chicken egg roll', 'white rice', 'brown rice',
-  'honey walnut shrimp'];
-  let burgerFoods = ['cheeseburger', 'hamburger', 'Cali burger', 'chili cheeseburger', 
-  'vegan burger', 'chicken sandwich', 'french fries', 'onion rings', 'double bacon cheeseburger',
-  'veggie burger', 'black bean burger', 'chicken tenders','southwest chicken salad'];
-  let koreanFoods = ['bulgogi beef', 'galbi chicken', 'beef short ribs', 'red chicken', 
-  'organic tofu', 'spicy pork', 'kimchi', 'noodles', 'kimchi rice', 'salad'];
+  let italianFoods = ['Cheese Pizza', 'Chicken Parmesan', 'Spaghetti and Meatballs', 'Garlic Bread', 
+  'Pasta with Pesto', 'Bread Sticks', 'Caesar Salad', 'Pepperoni Pizza', 'BBQ Pizza'];
+  let mexicanFoods = ['Fish Tacos', 'Chicken Tacos', 'Carne Asada Tacos', 
+  'Spicy Enchiladas', 'California Burrito', 'Shrimp Tacos', 'Fajita Salad Bowl', 'Veggie Bowl',
+   'Steak Quesadilla', 'Nacho Platter', 'Taquitos'];
+  let chineseFoods = ['Chow Mein', 'Chicken Fried Rice', 'Orange Chicken', 'Teriyaki Chicken', 
+  'Wok-Fired Shrimp', 'Kung Pao Chicken','Veggie Egg Roll', 'Chicken Egg Roll', 'White Rice', 'Brown Rice',
+  'Honey Walnut Shrimp'];
+  let burgerFoods = ['Cheeseburger', 'Hamburger', 'Cali Burger', 'Chili Cheeseburger', 
+  'Vegan Burger', 'Chicken Sandwich', 'French Fries', 'Onion Rings', 'Double Bacon Cheeseburger',
+  'Veggie Burger', 'Black Bean Burger', 'Chicken Tenders','Southwest Chicken Salad'];
+  let koreanFoods = ['Bulgogi Beef', 'Galbi Chicken', 'Beef Short Ribs', 'Red Chicken', 
+  'Organic Tofu', 'Spicy Pork', 'Kimchi', 'Spicy Noodles', 'Kimchi Rice', 'Chicken Salad'];
   
   let food;
   if (foodType === 'italian') {
@@ -37,7 +37,9 @@ const commentGenerator = (foodType, starRating) => {
   let positiveStart = ['I love eating from here! ', 'My family and I order from here twice a week. ',
   'I often order this on my lunch break. ', 'This is a great place to get a family dinner. ',
   'I have them cater my business meetings and my associates love it. ', 'Great for a quick bite. ', 
-  'They give you a lot of food for the price! ', 'Always on time!'];
+  'They give you a lot of food for the price! ', 'Always on time! ', 'Never disappoints. ',
+  'Best restaurant in town! ', 'I was able to use a great coupon. ','Awesome food here. ',
+  'They always get my order right. ', 'Thank you for the extra side. '];
   let neutralStart = ['This place is just okay. ', 'We order from here every once in a while. ', 
   'Food here is hit or miss. ', 'Some of the food is great. ', 'I like this place but dont order on Mondays. ',
   'Great for adults but not for kids. ', 'The food was good but they wont deliver alcohol. ',
@@ -46,7 +48,7 @@ const commentGenerator = (foodType, starRating) => {
   'They cancelled my order after I was waiting for an hour! ','Prices here are outrageous. ', 
   'They refuse to deliver because the chef thinks "his food is too good for delivery". ',
   'The food smells terrible every time. ', 'The staff was so rude! ', 
-  'I received my food late and it was as cold as the Night King'];
+  'I received my food late and it was as cold as the Night King. ',' Garbage every time. '];
 
   let positiveMid = [`My favorite is the ${randomFood1}. `, `I ate the ${randomFood1} and it was to die for. `, 
   `We ordered the ${randomFood1} and it was delivered right away. `, `I've never had ${randomFood1} that was so juicy. `,
@@ -54,15 +56,18 @@ const commentGenerator = (foodType, starRating) => {
   `You will never eat ${randomFood1} anywhere as good as they make it here. `, 
   `The ${randomFood1} tastes great with some hot sauce. `, 
   `I love the ${randomFood1}. It is the best thing on the menu. `,
-  `One time they gave me 3 extra orders of ${randomFood1} by accident. `,];
+  `Every time I order the ${randomFood1} I am reminded of my childhood when we would go camping, make a fire, and warm up some ${randomFood1}. `,
+  `One time they gave me 3 extra orders of ${randomFood1} by accident. `,
+  `We order the ${randomFood1} every week at my office and it's sooooo good. `];
   let neutralMid = [`The ${randomFood1} is good but everything else on the menu sucks. `,
   `If not for the half price ${randomFood1} special on Thursdays, I probably would not order from here. `,
   `My husband likes getting the ${randomFood1}. I'm not a huge fan. `,
   `I ate the ${randomFood1}. Not bad but nothing like the Cheesecake Factory. `,
   `They sent me a box of sushi when I really ordered ${randomFood1}, but they were nice enough to refund my order and give me free food. `,
-  `The ${randomFood1} spilled out of the container...but it was still delicious. `];
+  `The ${randomFood1} spilled out of the container. `];
   let negativeMid = [`There is so much salt in the ${randomFood1}. `, `There was a bug in my ${randomFood1}. `, 
-  `My son ate the ${randomFood1} and felt sick all night. `, 
+  `My son ate the ${randomFood1} and felt sick all night. `,
+  `You could tell someone had eaten part of the ${randomFood1} before I got it. `, 
   `I asked if the ${randomFood1} was made with dairy and the employee said no but there so much butter in it; I am lactose intolerant and I feel like I'm going to die. `,
   `I usually love ${randomFood1} but they don't know how to make it the right way. `,
   `The delivery man threw ${randomFood1} at my face. `,
@@ -70,15 +75,17 @@ const commentGenerator = (foodType, starRating) => {
 
   let positiveEnd = ['Would definitely recommend! Mmmmmm', 'I would give it 6 stars if I could.', 
   'Cant wait to order this again soon!', 'Best food ive had in a while!', 'Now I know what food should taste like.',
-  'My daughter even got a free dessert with her kids meal!', 'They were so nice they gave me a drink on the house.'];
+  'My daughter even got a free dessert with her kids meal!', 'They were so nice they gave me a drink on the house.',
+  'Five stars all the way!', 'Awesome service!', 'Loved it!', 'They deliver it so fast every time.'];
   let neutralEnd = ['I just wish they knew how to pack soup properly.', 'There are better options if you have time.',
   'It is too busy most nights', 'The food was great but it took way too long.', 
-  'It was better than I expected, but not great.', 'They forgot my drink.'];
-  let negativeEnd = ['I will never eat from here again!.', 'We will be reporting this restaurant to the BBB.',
+  'It was better than I expected, but not great.', 'They forgot my drink.', 'Meh.'];
+  let negativeEnd = ['I will never eat from here again!.', 'We will be reporting this restaurant to the BBB and FDA.',
   'Someone is going to get sick eating here.', 'AVOID AT ALL COSTS!!', 'Order from the place next door instead.',
   'I will be pursuing legal action immediately.', 'Im still hungry!', 'I think I may throw up.', 
-  'I would rather eat a hot dog at 7/11', 
-  'Every time I ask for extra sauce they put a note in my order that says "What do you think this is, the Bellagio? We dont have Kim Kardashian money".'];
+  'I would rather eat a hot dog at 7/11', 'They never get my order right',
+  `I tried calling the restaurant but their phone didn't work.`, 'I would not feed this to my dog.', 'Never again!',
+  'Every time I ask for extra sauce they put a note in my order that says "What do you think this is, the Bellagio? We dont have Kim Kardashian money here".'];
 
   let comment= '';
   let startPhrase;
@@ -103,19 +110,24 @@ const commentGenerator = (foodType, starRating) => {
   comment+= startPhrase;
   comment+= midPhrase;
   comment+= endPhrase;
-  console.log(comment);
   return comment;
 }
+
+let restaurantID = 1;
 
 const generateFakeData = (num) => {
   let fakeData = [];
   let _id = 0;
+
+  if (restaurantID > 100) {
+    restaurantID = 1;
+  }
+  
   for (let i = 0; i < num; i++) {
     _id++;
     let instance = {};
     instance.user = {};
 
-    let restaurantID = 5; ////////////////////////////FIX THIS
     let firstName = faker.name.firstName();
     let firstLetter = firstName[0];
     let numOfRatings = Math.ceil(Math.random() * 10);
@@ -145,6 +157,7 @@ const generateFakeData = (num) => {
     // console.log(_id);
 
     instance._id = _id;
+    instance.restaurantID = restaurantID;
     instance.user.firstName = firstName;
     instance.user.firstLetter = firstLetter;
     instance.user.numOfRatings = numOfRatings;
@@ -155,20 +168,20 @@ const generateFakeData = (num) => {
 
     fakeData.push(instance);
 
+    restaurantID++;
   }
 
   console.log(fakeData);
+  return fakeData;
 }
 
-generateFakeData(8);
-
-
+generateFakeData(10);
 
 
 /*
 {
 _id: Number (1-500)
-restaurantID: Number (1-100) //////////////
+restaurantID: Number (1-100)
 user: {
          Name: String
          firstLetter: String
