666487e506ed428879ca1c609935a97606f716ca
Ramin Rabani <rrabani2@gmail.com>
2019-05-20 16:00:26 -0700

    Match footer background color

--- client/Components/Footer/Footer.jsx
+++ client/Components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = (props) => {
     return (
       <div>
+        <div className="footer-test">
         Footer
+        </div>
       </div>
     )
 }
