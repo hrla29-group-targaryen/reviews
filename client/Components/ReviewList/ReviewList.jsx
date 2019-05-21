d79e15cccfedc0f8c2049b0839a56c8d53da2fad
Ramin Rabani <rrabani2@gmail.com>
2019-05-21 13:55:22 -0700

    Create dropdown option for sorting

--- client/Components/ReviewList/ReviewList.jsx
+++ client/Components/ReviewList/ReviewList.jsx
@@ -6,6 +6,16 @@ const ReviewList = (props) => {
   return (
     <div>
       <div className="reviewList-main">
+        <div className="reviewList-sort">
+          <div className="reviewList-sortText">Sort by:</div>
+          <div className="reviewList-select">
+            <select className="reviewList-selectBox">
+              <option value="0">Most recent</option>
+              <option value="1">Highest rated</option>
+            </select>
+            <img className="reviewList-dropdownArrow" src="grubhubDropdownArrow.png" alt="dropdownArrow"></img>
+          </div>
+        </div>
         {props.reviewData.map(function (item, index) {
           return <ReviewListItem review={item} key={index} />
         })}
