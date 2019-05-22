867575f633b8398e9fec092b950ecead1a20557a
Ramin Rabani <rrabani2@gmail.com>
2019-05-22 14:27:08 -0700

    Revise some CSS for footer

--- client/Components/Footer/Footer.jsx
+++ client/Components/Footer/Footer.jsx
@@ -3,53 +3,56 @@ import './footer.css';
 
 const Footer = (props) => {
   return (
-    <div className="footer-body footer-topHalf">
-      <div className="footer-collapsibleSection">
-        <span className="footer-collapsibletitleText">Get to know us</span>
-        <div className="footer-collapsibleContent">
-          <ul>
-            <li><a href="">About Grubhub</a></li>
-            <li><a href="">Our apps</a></li>
-            <li><a href="">Our blog</a></li>
-            <li><a href="">Careers</a></li>
-            <li><a href="">Investor relations</a></li>
-            <li><a href="">News</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-collapsibleSection">
-        <span className="footer-collapsibletitleText">Useful links</span>
-        <div className="footer-collapsibleContent">
-          <ul>
-            <li><a href="">Gift cards</a></li>
-            <li><a href="">Coupons</a></li>
-            <li><a href="">FAQ</a></li>
-            <li><a href="">Help</a></li>
-            <li><a href="">Grubhub for Alexa</a></li>
-            <li><a href="">Student discounts</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-collapsibleSection">
-        <span className="footer-collapsibletitleText">Connect with us</span>
-        <div className="footer-collapsibleContent">
-          <ul>
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">YouTube</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-collapsibleSection">
-        <span className="footer-collapsibletitleText">Partner with us</span>
-        <div className="footer-collapsibleContent">
-          <ul>
-            <li><a href="">For restaurants</a></li>
-            <li><a href="">For drivers</a></li>
-            <li><a href="">For corporate accounts</a></li>
-          </ul>
-        </div>
+    <div className="footer-body">
+      <div className="footer-topHalf">
+        <section className="footer-collapsibleSection col-1-4">
+          <span className="footer-collapsibletitleText">Get to know us</span>
+          <div className="footer-collapsibleContent">
+            <ul>
+              <li className="footer-li"><a className="footer-a" href="">About Grubhub</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Our apps</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Our blog</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Careers</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Investor relations</a></li>
+              <li className="footer-li"><a className="footer-a" href="">News</a></li>
+            </ul>
+          </div>
+        </section>
+        <section className="footer-collapsibleSection col-1-4">
+          <span className="footer-collapsibletitleText">Useful links</span>
+          <div className="footer-collapsibleContent">
+            <ul className="footer-ul">
+              <li className="footer-li"><a className="footer-a" href="">Gift cards</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Coupons</a></li>
+              <li className="footer-li"><a className="footer-a" href="">FAQ</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Help</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Grubhub for Alexa</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Student discounts</a></li>
+            </ul>
+          </div>
+        </section>
+        <section className="footer-collapsibleSection col-1-4">
+          <span className="footer-collapsibletitleText">Connect with us</span>
+          <div className="footer-collapsibleContent">
+            <ul>
+              <li className="footer-li"><a className="footer-a" href="">Facebook</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Twitter</a></li>
+              <li className="footer-li"><a className="footer-a" href="">Instagram</a></li>
+              <li className="footer-li"><a className="footer-a" href="">YouTube</a></li>
+            </ul>
+          </div>
+        </section>
+        <section className="footer-collapsibleSection col-1-4">
+          <span className="footer-collapsibletitleText">Partner with us</span>
+          <div className="footer-collapsibleContent">
+            <ul>
+              <li className="footer-li"><a className="footer-a" href="">For restaurants</a></li>
+              <li className="footer-li"><a className="footer-a" href="">For drivers</a></li>
+              <li className="footer-li"><a className="footer-a" href="">For corporate accounts</a></li>
+            </ul>
+          </div>
+        </section>
+
       </div>
       <div className="footer-bottomHalf">
         <div className="footer-divider">
@@ -58,11 +61,10 @@ const Footer = (props) => {
           <p>© 2019 Grubhub All rights reserved.</p>
         </div>
         <div>
-          <a href="">Terms of Use</a><br />
-          <a href="">Privacy Policy</a>
+          <a className="footer-a" href="">Terms of Use</a><br />
+          <a className="footer-a" href="">Privacy Policy</a>
         </div>
       </div>
-
     </div>
   )
 }
