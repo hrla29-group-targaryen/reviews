e7beb14950b17df4ba96cf710f22ae60f9d76e0a
Ramin Rabani <rrabani2@gmail.com>
2019-05-22 16:06:12 -0700

    Add tooltip popup window to ordered items

--- client/Components/ReviewList/ReviewListItem/OrderItem/OrderItem.jsx
+++ client/Components/ReviewList/ReviewListItem/OrderItem/OrderItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+
 import './orderItem.css';
 
 const OrderItem = (props) => {
+  let prices = ['$3.49', '$4.49', '$5.49', '$6.49', '$7.49', '$8.49', '$9.49', '$10.49', '$11.49', 
+  '$3.99', '$4.99', '$5.99', '$6.99', '$7.99', '$8.99', '$9.99', '$10.99', '$11.99', 
+  '$12.99', '$13.99', '$14.99', '$15.99',]
+  let randomPrice = prices[Math.floor(Math.random() * prices.length)];
+
   return (
     <div className="orderItem-order">
-      {props.order} +
+      <div className="orderItem-tooltip">{props.order} +
+        <div className="orderItem-tooltiptext">
+          <h6 className="orderItem-name">{props.order}</h6>
+          <div className="orderItem-price">{randomPrice}+</div>
+          <button className="orderItem-add">Add to bag</button>
+        </div>
+      </div>
     </div>
   )
 }
