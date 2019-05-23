3c3156c95205c7a3e0d4063bcccbfa6ab1a55603
Ramin Rabani <rrabani2@gmail.com>
2019-05-23 11:29:28 -0700

    Change font to roboto

--- client/Components/ReviewList/ReviewList.jsx
+++ client/Components/ReviewList/ReviewList.jsx
@@ -41,6 +41,7 @@ class ReviewList extends React.Component {
           {this.props.reviewData.map(function (item, index) {
             return <ReviewListItem review={item} key={index} />
           })}
+          <button className="reviewList-viewAllButton">View all reviews</button><br/>
           <div className="reviewList-bottom">
             <a className="reviewList-a" href="">Grubhub</a> / 
             <a className="reviewList-a" href="">{randomCity}</a> / 
