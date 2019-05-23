ab5b86802a4505159fa17bc9b27b53cd68a491b7
Ramin Rabani <rrabani2@gmail.com>
2019-05-22 23:53:17 -0700

    Create post request handling in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path');
+const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 const ReviewList = require ('../database/model.js');
 
 const app = express()
 const port = 3200
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 //serve the client with endpoint restaurant with specific restaurantID
 app.use('/restaurants/:restaurantID', express.static(path.join(__dirname, '../public')))
@@ -15,7 +19,18 @@ app.get('/restaurants/:restaurantID/api/reviews', (req, res) => {
 
   //GET data for one restaurant; i.e. send data back where {restaurantID : restaurantID}
   ReviewList.find({restaurantID: restaurantID})
-    .then(data => res.status(201).send(data))
+    .then(data => res.status(200).send(data))
+    .catch(err => res.status(404).send(err));
+})
+
+app.post('/restaurants/api/reviews', (req, res) => {
+  
+  const {Ordered, user, reviewID, restaurantID, starRating, comments, date} = req.body;
+
+  console.log(req.body)
+
+  ReviewList.create({Ordered, user, reviewID, restaurantID, starRating, comments, date})
+    .then(() => res.status(201).send('Success posting review'))
     .catch(err => res.status(404).send(err));
 })
 
