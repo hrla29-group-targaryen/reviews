0254478e3dcc87dcec003a19f40bc1b85bdca226
Ramin Rabani <rrabani2@gmail.com>
2019-05-29 16:24:01 -0700

    Deploy server to heroku

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const path = require('path');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 const ReviewList = require ('../database/model.js');
 const cors = require('cors');
 
 const app = express()
-const port = 3200
+const port = process.env.PORT || 3200;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 //serve the client with endpoint restaurant with specific restaurantID
 // app.use('/restaurants/:restaurantID', express.static(path.join(__dirname, '../public')))
