a33a3b628e51f0ba3e43939ce7a44447a9930587
Ramin Rabani <rrabani2@gmail.com>
2019-05-29 16:13:48 -0700

    Deploy database to mongoDB

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/grubhub_reviews', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://hackreactortest:hackreactortest@cluster0-ihnwo.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.once('open', ()=> console.log("Database is up and running on 27017"))
