042aae605ff0d1c9f6e0635b4fed9b1f2b13d1ac
Ramin Rabani <rrabani2@gmail.com>
2019-05-29 16:48:07 -0700

    Install env-cmd and set database password

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hackreactortest:hackreactortest@cluster0-ihnwo.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect(`mongodb+srv://hackreactortest:${process.env.DBPASSWORD}@cluster0-ihnwo.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.once('open', ()=> console.log("Database is up and running on 27017"))
